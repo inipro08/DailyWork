4fb0325d4a8e1cb7da7dab1edc3911d69024d03f
Dat Pham <datptsskpi@gmail.com>
2022-06-10 08:48:59 +0700

    Dev

--- app/src/main/java/com/inis/dailywork/data/model/DailyNote.kt
+++ app/src/main/java/com/inis/dailywork/data/model/DailyNote.kt
@@ -1,10 +1,11 @@
 package com.inis.dailywork.data.model
 
+import com.inis.dailywork.enum.NoteStatusType
 import java.io.Serializable
 
 data class NoteItem(
     val time: String = "",
     val title: String = "",
     val content: String = "",
-    val
+    val noteStatusType: NoteStatusType? = NoteStatusType.CREATE
 ) : Serializable
